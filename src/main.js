7ba49857d1b7073490c3d6ae7e14bde23222915f
Polina Meshkova <polina_i@bk.ru>
2023-01-22 07:42:13 +0700

    connected Vuex, created a data store, added a property for storing a list of goods in the basket, added displaying the number of goods in the basket in the header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 // import { message, question } from './alert';
 // import alertFunction from './alert_function';
@@ -8,6 +9,7 @@ import router from './router';
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount('#app');
