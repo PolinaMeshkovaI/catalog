9de167f99faa5cfe4d4a41d7d715e6f435c27ecb
Polina Meshkova <polina_i@bk.ru>
2023-01-24 08:31:26 +0700

    Implemented adding a product to the cart using mutation

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,21 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     cartProducts: [
-      {
-        productId: 1,
-        amount: 2,
-      }
+      {productId: 1, amount: 2}
     ]
+  },
+  mutations: {
+    addProductToCart(state, {productId, amount}){
+      const item = state.cartProducts.find(item => item.productId === productId);
+
+      if(item){
+        item.amount += amount;
+      } else {
+        state.cartProducts.push({
+          productId,
+          amount,
+        });
+      }
+    }
   }
 });
