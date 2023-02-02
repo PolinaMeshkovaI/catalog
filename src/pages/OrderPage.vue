7993be73e9e26e482c8212d9cd832bb1b9ffba39
Polina Meshkova <polina_i@bk.ru>
2023-02-02 08:28:49 +0700

    added checkout page, sending form data to the server and error handling

---
+++ src/pages/OrderPage.vue
@@ -0,0 +1,168 @@
+<template>
+<main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="cart.html">
+            Корзина
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Оформление заказа
+          </a>
+        </li>
+      </ul>
+
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">
+        {{$store.state.cartProducts.length}} {{ $store.state.cartProducts.length, ['товар', 'товара', 'товаров'] | wordDeclension }}
+      </span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST" @submit.prevent="order">
+        <div class="cart__field">
+          <div class="cart__data">
+
+            <BaseFormText v-model="formData.name" :error="formError.name" title="ФИО" placeholder="Введите ваше полное имя"></BaseFormText>
+
+            <BaseFormText v-model="formData.address" :error="formError.address" title="Адрес доставки" placeholder="Введите ваш адрес"></BaseFormText>
+
+            <BaseFormText v-model="formData.phone" :error="formError.phone" type="tel" title="Телефон" placeholder="Введите ваш телефон"></BaseFormText>
+
+            <BaseFormText v-model="formData.email" :error="formError.email" type="email" title="Email" placeholder="Введи ваш Email"></BaseFormText>
+
+            <BaseFormTextarea v-model="formData.comment" :error="formError.comment" title="Комментарий к заказу" placeholder="Ваши пожелания"></BaseFormTextarea>
+
+          </div>
+
+          <div class="cart__options">
+            <h3 class="cart__title">Доставка</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="delivery" value="0" checked="">
+                  <span class="options__value">
+                    Самовывоз <b>бесплатно</b>
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="delivery" value="500">
+                  <span class="options__value">
+                    Курьером <b>500 ₽</b>
+                  </span>
+                </label>
+              </li>
+            </ul>
+
+            <h3 class="cart__title">Оплата</h3>
+            <ul class="cart__options options">
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="pay" value="card">
+                  <span class="options__value">
+                    Картой при получении
+                  </span>
+                </label>
+              </li>
+              <li class="options__item">
+                <label class="options__label">
+                  <input class="options__radio sr-only" type="radio" name="pay" value="cash">
+                  <span class="options__value">
+                    Наличными при получении
+                  </span>
+                </label>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="cart__block">
+          <div v-if="$store.state.cartLoading" style="text-align: center"><img src="/img/loader_order.gif"></div>
+
+          <ul class="cart__orders">
+            <li class="cart__order" v-for="item in products" :key="item.productId">
+              <h3>{{ item.product.title }}</h3>
+              <b>  {{ item.product.price * item.amount | numberFormat }}₽</b>
+              <span>Артикул: {{item.product.id}}</span>
+            </li>
+          </ul>
+
+          <div class="cart__total">
+            <p>Доставка: <b>500 ₽</b></p>
+            <p>Итого: <b>{{ products.length}}</b> товара на сумму <b>{{ totalPrice | numberFormat}} ₽</b></p>
+          </div>
+
+          <button class="cart__button button button--primery" type="submit">
+            Оформить заказ
+          </button>
+        </div>
+        <div class="cart__error form__error-block" v-if="formErrorMessage">
+          <h4>Заявка не отправлена!</h4>
+          <p>
+            {{ formErrorMessage }}
+          </p>
+        </div>
+      </form>
+    </section>
+  </main>
+
+</template>
+
+<script>
+import BaseFormText from '@/components/BaseFormText';
+import BaseFormTextarea from '@/components/BaseFormTextarea';
+import numberFormat from '@/helpers/numberFormat';
+import wordDeclension from '@/helpers/wordDeclension';
+import { API_BASE_URL } from '@/config';
+import { mapGetters } from 'vuex';
+import axios from 'axios';
+
+  export default {
+    data(){
+      return{
+        formData: {},
+        formError: {},
+
+        formErrorMessage: '',
+      }
+    },
+    components: { BaseFormText, BaseFormTextarea },
+    filters: { numberFormat, wordDeclension },
+    computed:{
+    ...mapGetters({products: 'cartDetailProducts', totalPrice: 'cartTotalPrice', amountProducts: 'cartAmountProducts'})
+    },
+    methods: {
+      order(){
+        this.formError = {};
+        this.formErrorMessage = '';
+
+        axios
+          .post(API_BASE_URL + `/api/orders`, {
+            ...this.formData
+            }, {
+              params: {
+                userAccessKey: this.$store.state.userAccessKey
+              }
+          })
+          .then(response => {
+            this.$store.commit('resetCart')
+          })
+          .catch(error => {
+            this.formError = error.response.data.error.request || {};
+            this.formErrorMessage = error.response.data.error.message;
+          })
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,15 @@ import MainPage from '@/pages/MainPage';
 import NotFoundPage from '@/pages/NotFoundPage';
 import ProductPage from '@/pages/ProductPage';
 import CartPage from '@/pages/CartPage';
+import OrderPage from '@/pages/OrderPage';
 
 Vue.use(VueRouter);
 
 const routes = [
   {name: 'main', component: MainPage, path: '/'},
   {name: 'product', component: ProductPage, path: '/product/:id'},
-  {name: 'cart', component: CartPage, path: '/cart/'},
+  {name: 'cart', component: CartPage, path: '/cart'},
+  {name: 'order', component: OrderPage, path: '/order'},
   {name: 'notFound', component: NotFoundPage, path: '*'},
 
 ];
